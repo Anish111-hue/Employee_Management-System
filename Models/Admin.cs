4f0787d8da222426b5195e0df27e4137b580e2a6
Anish111-hue <51984984+Anish111-hue@users.noreply.github.com>
2024-10-26 13:06:59 -0500

    Add files via upload

---
+++ Models/Admin.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployeeManagementSystem.Models
+{
+    public class Admin
+    {
+        public int Id { get; set; }
+        public string Userid { get; set; }
+        public string Password { get; set; }
+    }
+}
